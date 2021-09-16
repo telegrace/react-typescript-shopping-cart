eff663c5e1e910ee6b87710d8274dfd7c8c3db7a
grace <telegrace@gmx.de>
2021-09-16 11:08:01 +0200

    Feat: replaced useState with useReducer

---
+++ src/components/Pizza.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { AppSetStateContext, useSetState, useStateDispatch } from "./AppState";
+import PizzaCSS from "./Pizza.module.css";
+
+interface Pizza {
+	id: number;
+	name: string;
+	description: string;
+	price: number;
+}
+
+interface Props {
+	pizza: Pizza;
+}
+
+const Pizza: React.FC<Props> = ({ pizza }) => {
+	const dispatch = useStateDispatch();
+	const handleAddToCartClick = () => {
+		dispatch({
+			type: "ADD_TO_CART",
+			payload: {
+				item: { id: pizza.id, name: pizza.name, price: pizza.price },
+			},
+		});
+	};
+	return (
+		<li className={PizzaCSS.container}>
+			<h2>{pizza.name}</h2>
+			<p>{pizza.description}</p>
+			<p>{pizza.price}</p>
+			<button type="button" onClick={handleAddToCartClick}>
+				Add to Cart
+			</button>
+		</li>
+	);
+};
+
+export default Pizza;
